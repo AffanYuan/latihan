8e38b4f92cd7a71ed0442174783f965726ee9996
AffanYuan <atrazyuan@gmail.com>
2023-01-05 15:13:56 +0700

    first commit

---
+++ lib/Login/SignUp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:latihan/main.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
