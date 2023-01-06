cbbb87ee52bf0f5e52765c745a42e1d829fa4cbf
fahrimuhlis <fahrimuhlisabdillah@gmail.com>
2023-01-06 15:00:08 +0700

    first commit

--- lib/Screen/Login/LoginPage.dart
+++ lib/Screen/Login/LoginPage.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:latihan/Screen/Login/SignUp.dart';
 import 'package:latihan/components/my_textfield.dart';
+import 'package:latihan/components/my_button.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({super.key});
 
-  // text editing control
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+// text editing control
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user method
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
+      body: Center(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+            children: [
               const SizedBox(
-                height: 50,
+                height: 30,
               ),
 
               SizedBox(
@@ -32,11 +37,10 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
 
-              const SizedBox(height: 50),
+              const SizedBox(height: 0),
 
-              //welcome back
               Text(
-                'Welcome Back you',
+                'Welcome Back to You',
                 style: TextStyle(
                   color: Colors.grey[700],
                   fontSize: 16,
@@ -46,6 +50,24 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 25),
 
               //username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              //password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              //forgot password
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -61,44 +83,33 @@ class _LoginPageState extends State<LoginPage> {
 
               const SizedBox(height: 25),
 
-              // or continue
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
+              // sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 30),
+
+              // sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Don't Have an Account? "),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => SignUp()));
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-
-                    const SizedBox(height: 25),
-
-                    //username
-                    // MyTextField(
-                    //   controller:usernameController,
-                    //   hintText: 'Username',
-                    //   obscureText: false,
-                    // ),
-
-                    // const SizedBox(height: 10),
-
-                    // MyTextField(
-                    //   controller: passwordController,
-                    //   hintText: 'Password',
-                    //   obscureText: true,
-                    // ),
-                  ],
-                ),
-              )
+                  ),
+                ],
+              ),
             ],
           ),
         ),
